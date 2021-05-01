a8d9c0f6ee8f3bbd8f660821997d1240b13f8e92
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-05-01 23:20:59 +0300

    feat: add burer menu

--- online-zoo/pages/Zoos/js/script.js
+++ online-zoo/pages/Zoos/js/script.js
@@ -2,6 +2,8 @@ const parts =document.querySelectorAll('.part');
 const switcher = document.querySelector('header .checkbox');
 const donate =document.querySelectorAll('.formbtn');
 const donend =document.querySelector('.donate-form-btn')
+const burger =document.querySelectorAll('.burger');
+const burgerM =document.querySelector('.BurgerUl');
 
 function switchteme () {
   if (switcher.checked){
@@ -17,8 +19,13 @@ function EndDonate(){
     document.querySelector('.form-page').classList.add("displayNone")   
 }
 
+function burgermenu(){
+  if (burgerM.classList[2]=="activeB")  
+  {burgerM.classList.remove("activeB")}
+  else{burgerM.classList.add("activeB")}
+}
 
-
+burger.forEach(burger => burger.addEventListener('click',burgermenu));
 switcher.addEventListener('click',switchteme)
 donate.forEach(donate => donate.addEventListener('click',donateform));
 donend.addEventListener('click',EndDonate)
