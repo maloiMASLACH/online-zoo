c5b3c3f728d91bd9ed90df10cfb05ba5ad8b2065
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-05-02 19:46:05 +0300

    feat: add firs slider

--- online-zoo/pages/Landing/js/script.js
+++ online-zoo/pages/Landing/js/script.js
@@ -4,6 +4,9 @@ const donate =document.querySelectorAll('.formbtn');
 const donend =document.querySelector('.donate-form-btn')
 const burger =document.querySelectorAll('.burger');
 const burgerM =document.querySelector('.BurgerUl');
+const anaimalSlider1 =document.querySelectorAll('.animalsOnSlider ul li');
+const input1 = document.querySelector('.ponzynokNull');
+const output1 =document.querySelector('.counternull output' );
 
 function switchteme () {
   if (switcher.checked){
@@ -16,14 +19,88 @@ function donateform(){
 document.querySelector('.form-page').classList.remove("displayNone")
 }
 function EndDonate(){
-    document.querySelector('.form-page').classList.add("displayNone")   
+    document.querySelector('.form-page').classList.add("displayNone")  
+ //   alert("Operation comleate. Thank You") 
 }
 function burgermenu(){
   if (burgerM.classList[2]=="activeB")  
   {burgerM.classList.remove("activeB")}
   else{burgerM.classList.add("activeB")}
 }
+function anaimalSlider1perehodPoClick(e){
+  anaimalSlider1.forEach(anaimalSlider1 =>{
+    anaimalSlider1.classList.remove("active")
+    
+  })
+  e.path[1].classList.add("active")
+  input1.value=e.path[1].value
+  output1.value='0'+e.path[1].value
+ 
+  let shift;
 
+  if (window.screen.width < 640) {
+    shift = 100;
+  } else if (window.screen.width < 1920) {
+    shift = 190;
+  } else {
+    shift = 166;
+  }
+
+  document.querySelector(".animalsOnSlider ul").style.transform = `translateX(-${(input1.value - 2) * shift}px)`;
+    for (let i = 0; i < input1.value - 2; i++) {
+      if (anaimalSlider1[i] && !anaimalSlider1[i].classList.contains("hide-slide")) {
+        anaimalSlider1[i].classList.add("hide-slide");
+      }
+    }
+   
+    document.querySelector(".animalsOnSlider ul").style.transform = `translateX(${(2 - input1.value) * shift}px)`;
+    for (let i = input1.value - 2; i < anaimalSlider1.length; i++) {
+      if (anaimalSlider1[i] && anaimalSlider1[i].classList.contains("hide-slide")) {
+        anaimalSlider1[i].classList.remove("hide-slide");
+      }
+    }
+ 
+
+
+}
+
+
+
+function slider1(){
+  anaimalSlider1.forEach(anaimalSlider1 =>{
+    anaimalSlider1.classList.remove("active")
+  })
+  anaimalSlider1[input1.value-1].classList.add("active")
+  let shift;
+  if (window.screen.width < 320) {
+    shift = 122;
+  } else  if (window.screen.width < 640) {
+    shift = 122;
+  } else if (window.screen.width < 1920) {
+    shift = 190;
+  } else {
+    shift = 186;
+  }
+
+
+    document.querySelector(".animalsOnSlider ul").style.transform = `translateX(-${(input1.value - 2) * shift}px)`;
+    for (let i = 0; i < input1.value - 2; i++) {
+      if (anaimalSlider1[i] && !anaimalSlider1[i].classList.contains("hide-slide")) {
+        anaimalSlider1[i].classList.add("hide-slide");
+      }
+    }
+   
+    document.querySelector(".animalsOnSlider ul").style.transform = `translateX(${(2 - input1.value) * shift}px)`;
+    for (let i = input1.value - 2; i < anaimalSlider1.length; i++) {
+      if (anaimalSlider1[i] && anaimalSlider1[i].classList.contains("hide-slide")) {
+        anaimalSlider1[i].classList.remove("hide-slide");
+      }
+    }
+  
+}
+
+input1.addEventListener('input',slider1)
+anaimalSlider1.forEach((anaimalSlider1) => anaimalSlider1.addEventListener('click',anaimalSlider1perehodPoClick));
 burger.forEach(burger => burger.addEventListener('click',burgermenu));
 switcher.addEventListener('click',switchteme)
 donate.forEach(donate => donate.addEventListener('click',donateform));
