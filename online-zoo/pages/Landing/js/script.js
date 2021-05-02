6ca8f07b7ce1cbce3003b095502a77aba8d7962a
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-05-02 19:54:42 +0300

    feat: corrected margins

--- online-zoo/pages/Landing/js/script.js
+++ online-zoo/pages/Landing/js/script.js
@@ -73,7 +73,7 @@ function slider1(){
   anaimalSlider1[input1.value-1].classList.add("active")
   let shift;
   if (window.screen.width < 320) {
-    shift = 152;
+    shift = 192;
   } else  if (window.screen.width < 640) {
     shift = 122;
   } else if (window.screen.width < 1920) {
